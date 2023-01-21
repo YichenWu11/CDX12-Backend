1ae3c5750b0187bab87d3e85623b02c3b5d0ac90
WuYichen <2950335254@qq.com>
2023-01-21 23:20:37 +0800

    minor

--- include/CDX12/Shader/BasicShader.h
+++ include/CDX12/Shader/BasicShader.h
@@ -27,28 +27,28 @@ namespace Chen::CDX12 {
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                vsShader = DXUtil::CompileShader(path, shaderDefines.data(), "VS", "vs_5_1");
+                vsShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "VSMain", "vs_5_1"));
         }
 
         void SetPsShader(
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                psShader = DXUtil::CompileShader(path, shaderDefines.data(), "PS", "ps_5_1");
+                psShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "PSMain", "ps_5_1"));
         }
 
         void SetHsShader(
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                hsShader = DXUtil::CompileShader(path, shaderDefines.data(), "HS", "hs_5_1");
+                hsShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "HSMain", "hs_5_1"));
         }
 
         void SetDsShader(
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                dsShader = DXUtil::CompileShader(path, shaderDefines.data(), "DS", "ds_5_1");
+                dsShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "DSMain", "ds_5_1"));
         }
 
         void SetRasterizerState(D3D12_RASTERIZER_DESC desc) { rasterizerState = desc; }
