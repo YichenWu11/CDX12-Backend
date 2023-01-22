554fc158c6b41f558f8ec537d4c89a065fdb5dfe
WuYichen <2950335254@qq.com>
2023-01-23 00:02:55 +0800

    remove useless std::move

--- include/CDX12/Shader/BasicShader.h
+++ include/CDX12/Shader/BasicShader.h
@@ -27,28 +27,28 @@ namespace Chen::CDX12 {
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                vsShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "VSMain", "vs_5_1"));
+                vsShader = DXUtil::CompileShader(path, shaderDefines.data(), "VSMain", "vs_5_1");
         }
 
         void SetPsShader(
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                psShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "PSMain", "ps_5_1"));
+                psShader = DXUtil::CompileShader(path, shaderDefines.data(), "PSMain", "ps_5_1");
         }
 
         void SetHsShader(
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                hsShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "HSMain", "hs_5_1"));
+                hsShader = DXUtil::CompileShader(path, shaderDefines.data(), "HSMain", "hs_5_1");
         }
 
         void SetDsShader(
             const wchar_t*                       path,
             const std::vector<D3D_SHADER_MACRO>& shaderDefines = std::vector<D3D_SHADER_MACRO>()) {
             if (path != nullptr)
-                dsShader = std::move(DXUtil::CompileShader(path, shaderDefines.data(), "DSMain", "ds_5_1"));
+                dsShader = DXUtil::CompileShader(path, shaderDefines.data(), "DSMain", "ds_5_1");
         }
 
         void SetRasterizerState(D3D12_RASTERIZER_DESC desc) { rasterizerState = desc; }
